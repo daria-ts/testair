f6908cc6108bea53a768f53247ba695ffaf7aab0
daria <tochdz@gmail.com>
2021-01-21 01:07:25 +0300

    fonts

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,6 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import "./static/typography.css"
 
 import Header from "./header"
 import "./layout.css"

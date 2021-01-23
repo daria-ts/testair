549c1a3ee11cfcf86bab555dcbf03f1a21f3ff37
daria <tochdz@gmail.com>
2021-01-23 07:01:03 +0300

    theme provider

--- src/themes/theme.js
+++ src/themes/theme.js
@@ -1,18 +1,20 @@
-import spacing from 'src/themes/tokens/spacing';
-import colors from 'src/themes/tokens/colors';
-import borderWidths from 'src/themes/tokens/borderWidths';
-import radii from 'src/themes/tokens/radii';
-import shadows from 'src/themes/tokens/shadows';
-import fontSizes from 'src/themes/tokens/fontSizes';
-import fontFamilies from 'src/themes/tokens/fontFamilies';
-import fontWeights from 'src/themes/tokens/fontWeights';
-import lineHeights from 'src/themes/tokens/lineHeights';
-import delays from 'src/themes/tokens/delays';
-import durations from 'src/themes/tokens/durations';
-import easings from 'src/themes/tokens/easings.ts';
-import letterSpacings from 'src/themes/tokens/letterSpacings.ts';
-import mediaQueries from '/src/themes/tokens/mediaQueries.ts';
-import opacities from 'src/themes/tokens/opacities.ts';
-import zIndices from 'src/themes/tokens/zIndices.ts';
+import spacing from './src/themes/tokens/spacing';
+import colors from './src/themes/tokens/colors';
+import borderWidths from './src/themes/tokens/borderWidths';
+import radii from './src/themes/tokens/radii';
+import shadows from './src/themes/tokens/shadows';
+import fontSizes from './src/themes/tokens/fontSizes';
+import fontFamilies from './src/themes/tokens/fontFamilies';
+import fontWeights from './src/themes/tokens/fontWeights';
+import lineHeights from './src/themes/tokens/lineHeights';
+import delays from './src/themes/tokens/delays';
+import durations from './src/themes/tokens/durations';
+import easings from './src/themes/tokens/easings.ts';
+import letterSpacings from './src/themes/tokens/letterSpacings.ts';
+import mediaQueries from '/./src/themes/tokens/mediaQueries.ts';
+import opacities from './src/themes/tokens/opacities.ts';
+import zIndices from './src/themes/tokens/zIndices.ts';
 
-export default theme;
+const background =
+
+export default Theme;

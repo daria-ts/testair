5a976362ca0239a123be162a85ef40f6ead39176
daria <tochdz@gmail.com>
2021-01-23 11:01:25 +0300

    1

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { ThemeProvider } from "styled-components"
-import Theme from "./src/themes/theme"
+import Theme from "./src/components/theme"
 
 export const wrapRootElement = ({ element }) => (
   <ThemeProvider theme={Theme}>{element}</ThemeProvider>

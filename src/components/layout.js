22a6be81d3eed4ff289c9b29e3c6ca25a43f2612
daria <tochdz@gmail.com>
2021-01-23 10:14:35 +0300

    1

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,8 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
-import "./layout.css"
+import ".src/components/tokens/colors.js"
+//import "./layout.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -32,6 +33,7 @@ const Layout = ({ children }) => {
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
+        <GlobalStyle />
         <main>{children}</main>
         <footer
           style={{

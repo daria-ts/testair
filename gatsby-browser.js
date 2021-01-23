377a61d34ebeaa841805bfd7ef32d161c30dba95
daria <tochdz@gmail.com>
2021-01-23 07:12:30 +0300

    bug fixed

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,6 @@ import React from "react"
 import {ThemeProvider} from "styled-components"
 import Theme from "./src/themes/theme"
 
-export const wrapRootElement = { element } => (
+export const wrapRootElement = ({ element }) => (
     <ThemeProvider theme={Theme}>{element}</ThemeProvider>
 )

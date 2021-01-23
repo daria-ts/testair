bc4deb70b578873675217ed07d0ad54f6077cc33
daria <tochdz@gmail.com>
2021-01-23 08:18:32 +0300

    1

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
 //import "./layout.css"
-import "./src/themes/global-style.js"
+import "./src/themes.global-style.js"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`

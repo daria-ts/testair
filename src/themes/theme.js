7b7eaf3312a2f4ee3d1face31863df13a5cf59a3
daria <tochdz@gmail.com>
2021-01-23 07:10:43 +0300

    test bg color

--- src/themes/theme.js
+++ src/themes/theme.js
@@ -1,20 +1,25 @@
-import spacing from './src/themes/tokens/spacing';
-import colors from './src/themes/tokens/colors';
-import borderWidths from './src/themes/tokens/borderWidths';
-import radii from './src/themes/tokens/radii';
-import shadows from './src/themes/tokens/shadows';
-import fontSizes from './src/themes/tokens/fontSizes';
-import fontFamilies from './src/themes/tokens/fontFamilies';
-import fontWeights from './src/themes/tokens/fontWeights';
-import lineHeights from './src/themes/tokens/lineHeights';
-import delays from './src/themes/tokens/delays';
-import durations from './src/themes/tokens/durations';
-import easings from './src/themes/tokens/easings.ts';
-import letterSpacings from './src/themes/tokens/letterSpacings.ts';
-import mediaQueries from '/./src/themes/tokens/mediaQueries.ts';
-import opacities from './src/themes/tokens/opacities.ts';
-import zIndices from './src/themes/tokens/zIndices.ts';
+import { createGlobalStyle } from 'styled-components'
+import reset from 'styled-reset'
+import spacing from './src/themes/tokens/spacing.js'
+import colors from './src/themes/tokens/colors.js'
+import borderWidths from './src/themes/tokens/borderWidths.js'
+import radii from './src/themes/tokens/radii.js'
+import shadows from './src/themes/tokens/shadows.js'
+import fontSizes from './src/themes/tokens/fontSizes.js'
+import fontFamilies from './src/themes/tokens/fontFamilies.js'
+import fontWeights from './src/themes/tokens/fontWeights.js'
+import lineHeights from './src/themes/tokens/lineHeights.js'
+import delays from './src/themes/tokens/delays.js'
+import durations from './src/themes/tokens/durations.js'
+import easings from './src/themes/tokens/easings.js'
+import letterSpacings from './src/themes/tokens/letterSpacings.js'
+import mediaQueries from '/./src/themes/tokens/mediaQueries.js'
+import opacities from './src/themes/tokens/opacities.js'
+import zIndices from './src/themes/tokens/zIndices.js';
 
-const background =
+body {
+    backgroundColor: '#f7fafc';
+  }
+
 
 export default Theme;

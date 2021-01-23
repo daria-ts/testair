299bdd45f01657f96300fed82ea9b752d6bfbb6c
daria <tochdz@gmail.com>
2021-01-23 07:22:47 +0300

    1

--- src/themes/theme.js
+++ src/themes/theme.js
@@ -1,22 +1,7 @@
 import { createGlobalStyle } from 'styled-components'
-import spacing from './src/themes/tokens/spacing.js'
-import colors from './src/themes/tokens/colors.js'
-import borderWidths from './src/themes/tokens/borderWidths.js'
-import radii from './src/themes/tokens/radii.js'
-import shadows from './src/themes/tokens/shadows.js'
-import fontSizes from './src/themes/tokens/fontSizes.js'
-import fontFamilies from './src/themes/tokens/fontFamilies.js'
-import fontWeights from './src/themes/tokens/fontWeights.js'
-import lineHeights from './src/themes/tokens/lineHeights.js'
-import delays from './src/themes/tokens/delays.js'
-import durations from './src/themes/tokens/durations.js'
-import easings from './src/themes/tokens/easings.js'
-import letterSpacings from './src/themes/tokens/letterSpacings.js'
-import mediaQueries from '/./src/themes/tokens/mediaQueries.js'
-import opacities from './src/themes/tokens/opacities.js'
-import zIndices from './src/themes/tokens/zIndices.js'
 
-body = {
+
+body = { 
     backgroundColor: '#f7fafc'
   }
 

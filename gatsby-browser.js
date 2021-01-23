d89bf0d7b4ca71fcbee431a7128f7c23cfc8078b
daria <tochdz@gmail.com>
2021-01-23 07:50:41 +0300

    reset installed for styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 import React from "react"
-import {ThemeProvider} from "styled-components"
-import Theme from "./src/themes/theme"
+import { ThemeProvider } from "styled-components"
+import Theme from "./src/themes/global-style"
 
 export const wrapRootElement = ({ element }) => (
-    <ThemeProvider theme={Theme}>{element}</ThemeProvider>
+  <ThemeProvider theme={Theme}>{element}</ThemeProvider>
 )
